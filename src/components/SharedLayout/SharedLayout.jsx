d99ffc5a949a47905f2e8b19eb5eb5343d72917a
Nataliia D <113416916+SambykaLight@users.noreply.github.com>
2023-08-19 00:48:33 +0200

    BGI

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,19 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Container } from '@mui/material';
+import { Section } from './SharedLayout.styled';
 import Header from 'components/Header/Header';
-// import { Section } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
-    <div>
+    <>
         <Header/>
+        <Section>
         <Suspense fallback={<p>Loading...</p>}>
           <Container fixed>
             <Outlet />
           </Container>
         </Suspense>
-      </div>
+        </Section>
+      </>
   );
 };
 export default SharedLayout;
