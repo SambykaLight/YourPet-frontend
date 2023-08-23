bffee022be3da0cdd6a6a0914a551c8c16cc6b65
Taras-Veremchuk <taras.veremm@gmail.com>
2023-08-24 00:46:00 +0200

    added dispatch operation for registration

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,10 +1,10 @@
 import { useAuth } from 'hooks';
 import { Navigate } from 'react-router-dom';
 
-export const PublicRoute = ({ component: Component, redirectTo = '/' }) => {
+ const PublicRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn } = useAuth();
 
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
-//hello
 
+export default PublicRoute;
