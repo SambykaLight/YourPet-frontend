b7bfe883f87b59569a16012fa5f90cf454301c42
VitaliyOl <oliferchuk@gmail.com>
2023-08-23 16:37:03 +0300

    authorization complette

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import LoginForm from '../components/LoginForm';
 
 function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+  return <LoginForm />;
 }
 
-export default LoginPage
+export default LoginPage;
