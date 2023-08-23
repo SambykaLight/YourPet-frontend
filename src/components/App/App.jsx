a0a539c44203bcb4a1699369aad85a43dc16cc7b
Taras-Veremchuk <taras.veremm@gmail.com>
2023-08-23 23:09:35 +0200

    added private route and public route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
+import { PrivateRoute } from 'components/PrivateRoute/PrivateRoute';
+import { PublicRoute } from 'components/PublicRoute/PublicRoute';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import { lazy } from 'react';
+import { useAuth } from 'hooks/useAuth';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import authOperations from 'redux/auth/operations';
 
 const LoginPage = lazy(() => import('pages/LoginPage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage'));
@@ -13,17 +18,26 @@ const ErrorPage = lazy(() => import('pages/ErrorPage'));
 const NewsPage = lazy(() => import('pages/NewsPage'));
 
 export const App = () => {
-  return(
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+  useEffect(() => {
+    dispatch(authOperations.refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
   <Routes>
     <Route path="/" element={<SharedLayout />}>
       <Route index element={<HomePage />} />
-      <Route path="login" element={<LoginPage />} />
-      <Route path="register" element={<RegisterPage />} />
-
-      <Route path="user" element={<UserPage />} />
-
-      <Route path="add-pet" element={<AddPetPage />} />
-
+      <Route element={<PublicRoute />}>
+              <Route path="register" element={<RegisterPage />} />
+              <Route path="login" element={<LoginPage />} />
+            </Route>
+      <Route element={<PrivateRoute />}>
+              <Route path="user" element={<UserPage />} />
+              <Route path="add-pet" element={<AddPetPage />} />
+      </Route>
       <Route path="notices/" element={<PetsListPage />} />
 
       <Route path="notices/:category" element={<PetsListPage />} />
---
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,10 @@
+import { useAuth } from 'hooks';
+import { Navigate } from 'react-router-dom';
+
+export const PublicRoute = ({ component: Component, redirectTo = '/' }) => {
+  const { isLoggedIn } = useAuth();
+
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
+
+
