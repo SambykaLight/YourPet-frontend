74135e518ef5fad58c2e2c6bb2fa95bfc8a7e3b8
Roman Lebedynskyi <108210477+Anasurim@users.noreply.github.com>
2023-08-27 16:25:42 +0200

    header update
    -added svgs: bm icon, bm close icon, user icon on mobile
    -update authNav on mobile, display none to both auth buttons on mobile

---
+++ src/components/AuthNav/burgerMenuIcon.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export default function BurgerMenuIcon() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 7H20M4 12H20M4 17H20"
+        stroke="#ffc107"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
