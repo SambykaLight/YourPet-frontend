505d2a739fc7637bc8ae23a863894dfe84a20117
Nataliia D <113416916+SambykaLight@users.noreply.github.com>
2023-08-27 13:29:05 +0200

    hhh

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,16 +2,19 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Header from 'components/Header/Header';
 import Loader from 'components/Loader/Loader';
+import { Section,ContainerWalls } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
-      <div>
+      <Section>
         <Suspense fallback={<Loader/>}>
+      <ContainerWalls>
           <Outlet />
+      </ContainerWalls>
         </Suspense>
-      </div>
+        </Section>
     </>
   );
 };
