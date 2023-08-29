ca325aadf80c629aff035df25962c2f7b8043a43
SambykaLight <113416916+SambykaLight@users.noreply.github.com>
2023-08-29 14:01:50 +0000

    Deploying to gh-pages from @ SambykaLight/YourPet-frontend@9a427f6864d2e86719eb9b4f0fdb96e02d0709a4 🚀

--- static/js/150.8a8c090a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[150],{3150:function(e,t,r){r.r(t);r(2791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"NewsPage"})}}}]);
-//# sourceMappingURL=150.8a8c090a.chunk.js.map
