3557e28044d54d595e5425e7905e0f2a64ab3ba1
Taras-Veremchuk <taras.veremm@gmail.com>
2023-09-01 16:17:53 +0200

    add filtered notices

--- src/components/Notices/PetsList/PetsList.jsx
+++ src/components/Notices/PetsList/PetsList.jsx
@@ -3,17 +3,23 @@ import { List } from './PetsList.styled';
 import { useSelector } from 'react-redux';
 import noticeSelectors from 'redux/notices/selectors';
 import Context from 'components/Modals/Context/Context';
+import { useParams } from 'react-router-dom';
 
 const CategoryList = () => {
-  const notices = useSelector(noticeSelectors.selectNotices);
 
+  const notices = useSelector(noticeSelectors.selectNotices);
+const {category} = useParams()
+const filteredNotices = notices.filter(notice=> notice.category === category)
   return (
     <Context>
-      <List>
-        {notices.map(card => (
+   { filteredNotices.length > 0 ?
+      (<List>
+        {filteredNotices.map(card => (
           <CategoryItem {...card} key={card._id} />
         ))}
-      </List>
+      </List>): ( <div>
+  <p>List is empty, please add pet</p>
+    </div>)}
     </Context>
   );
 };
