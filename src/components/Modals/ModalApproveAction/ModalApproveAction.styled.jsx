1a04ea47dd9eceea19eb60781dc0a729e8b89693
Nataliia <113416916+SambykaLight@users.noreply.github.com>
2023-09-04 16:26:16 +0200

    Revert "fix/ModalCongrats /ModalApproveAction"

--- src/components/Modals/ModalApproveAction/ModalApproveAction.styled.jsx
+++ src/components/Modals/ModalApproveAction/ModalApproveAction.styled.jsx
@@ -1,54 +1,44 @@
 import styled from 'styled-components';
-import { RxCross2 } from 'react-icons/rx';
+// import { CiLogout } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 
-
 export const SectionApprove = styled.div`
-width: 280px;
-@media (min-width: 768px) {
-width: 608px;
-}
+  width: 280px;
+  @media (min-width: 768px) {
+    width: 608px;
+  }
 `;
 
 export const HeaderApprove = styled.p`
-
-    ont-family: 'Manrope';
-font-weight: 500;
-font-size: 24px;
-line-height: 33px;
-text-align: center; 
-color:var(----dark-color);
-margin-top: 68px;
-margin-bottom: 50px;
-    @media (min-width: 768px) {
-        font-size: 36px;
-        line-height: 49px;
-        margin-top: 60px;
-}
-`;
-export const BtnsWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  ont-family: 'Manrope';
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 33px;
+  text-align: center;
+  color: var(----dark-color);
+  margin-top: 68px;
+  margin-bottom: 50px;
   @media (min-width: 768px) {
-    flex-direction: row;
     font-size: 36px;
     line-height: 49px;
     margin-top: 60px;
   }
+`;
+export const BtnsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 export const BtnCancel = styled.button`
   display: flex;
   flex-direction: row;
   justify-content: center;
-//  align-items: center;
+  align-items: center;
   padding: 8px 28px;
-  margin-right: 0;
-  margin-left: 0;
-  width: 256px;
-
+  margin-right: 167 px;
+  margin-left: 7px;
+  width: 129px;
+  height: 40px;
   color: #54adff;
   outline: none;
   background-color: transparent;
@@ -58,37 +48,28 @@ export const BtnCancel = styled.button`
   font-weight: 700;
   font-size: 16px;
   line-height: 22px;
-  
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: center;
-  margin-right: 167 px;
-  margin-left: 7px;
-  width: 129px;
-  height: 40px;
-  
   margin-bottom: 60px;
-  cursor: pointer;  
-    &:hover {
+  cursor: pointer;
+  &:hover {
     background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
     color: #fef9f9;
-}  
-&:focus {
+  }
+  &:focus {
     background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
     color: #fef9f9;
-}
+  }
 `;
 
 export const BtnYes = styled(Link)`
   display: flex;
-
-  width: 256px;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 8px 28px;
-  margin-right: 0 px;
-  margin-left: 0px;
-  margin-top: 7px;
+  margin-right: 167 px;
+  margin-left: 7px;
+  width: 129px;
+  height: 40px;
   color: #54adff;
   outline: none;
   background-color: transparent;
@@ -99,63 +80,13 @@ export const BtnYes = styled(Link)`
   font-size: 16px;
   line-height: 22px;
   margin-bottom: 60px;
-  background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
-  color: #fef9f9;
-  
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: center;
-    margin-top: 0px;
-  margin-right: 167 px;
-  margin-left: 7px;
-  width: 129px;
-  height: 40px;
-
   cursor: pointer;
-  
-
   &:hover {
     background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
     color: #fef9f9;
-    }
-    &:focus {
-    background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
-    color: #fef9f9;
-    }
-`;
-
-
-export const StyledRxCross2 = styled(RxCross2)`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  position: relative;
-  stroke: currentColor; 
-  stroke-width: 1.5px;
-  stroke-linecap: round;
-  stroke-linejoin: round;
-  fill: none;
-  border-radius: 50%;
-  transition: stroke 250ms cubic-bezier(0.4, 0, 0.2, 1);
-
-  &:hover {
-    color: #fdf7f2;
   }
-`;
-
-
-
-export const CloseButton = styled.button`
-position: absolute;
-top: 16px;
-right: 16px;
-align-self: end;
-background-color: transparent;
-border: none;
-cursor: pointer;
-padding: 5px;
-&:hover {
-  color:var(--blue-color);
-}
-
+  &:focus {
+    background: linear-gradient(290.46deg, #419ef1 0%, #9bd0ff 107.89%);
+    color: #fef9f9;
+  }
 `;
--- src/components/Modals/ModalCongrats/ModalCongrats.jsx
+++ src/components/Modals/ModalCongrats/ModalCongrats.jsx
@@ -1,46 +1,28 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { RxCross2 } from 'react-icons/rx';
-import { LiaPawSolid } from 'react-icons/lia';
 import {
   CongratsContainer,
   TitleCongrats,
   TextCongrats,
-  CloseButton,
   ButtonCongrat,
-
 } from './ModalCongrats.styled';
 
 const ModalCongrats = ({ closeModal }) => {
-  const pageNavigate = useNavigate();
-  
-
   const handleClose = () => {
-      closeModal();
-      pageNavigate('/');
+    closeModal();
+    localStorage.setItem('modalCongratsAlreadyOpened', 'true');
   };
 
-
-  
   return (
-    <CongratsContainer  onClick={handleClose} >
-              <CloseButton  onClick={handleClose}>
-          <RxCross2 />
-        </CloseButton>
+    <CongratsContainer closeModal={handleClose}>
       <TitleCongrats>Congrats!</TitleCongrats>
-      <TextCongrats>Your registration is success
-        
-      </TextCongrats>
+      <TextCongrats>Your registration is success</TextCongrats>
       <ButtonCongrat
         onClick={handleClose}
         className="btn"
         type="button"
         color="blue"
         width="248px"
-        
-      >Go to profile 
-      <LiaPawSolid />
-      </ButtonCongrat>
+      >Go To Profile</ButtonCongrat>
     </CongratsContainer>
   );
 };
